c3c9102a5fe4a6d63c4b834e61ee425704b95183
Arif6474 <arifulislam64743@gmail.com>
2023-06-03 12:15:21 +0600

    changed

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,55 +1,56 @@
 import { createLogger, format, transports } from 'winston'
 import path from 'path'
 import DailyRotateFile from 'winston-daily-rotate-file'
-const { combine, timestamp, label, printf } = format;
-
+const { combine, timestamp, label, printf } = format
 
 const myFormat = printf(({ level, message, label, timestamp }) => {
   const date = new Date(timestamp)
   const hour = date.getHours()
   const minute = date.getMinutes()
   const second = date.getSeconds()
-  return `${date.toDateString()} ${hour}:${minute}:${second} [${label}] ${level}: ${message}`;
-});
+  return `${date.toDateString()} ${hour}:${minute}:${second} [${label}] ${level}: ${message}`
+})
 
 //custom logger
 
 const logger = createLogger({
   level: 'info',
-  format: combine(
-    label({ label: 'HUC' }),
-    timestamp(),
-    myFormat,
- 
-  ),
+  format: combine(label({ label: 'HUC' }), timestamp(), myFormat),
   transports: [
     new transports.Console(),
     new DailyRotateFile({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'successes', 'huc%DATE%success.log'),
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'successes',
+        'huc%DATE%success.log'
+      ),
       datePattern: 'YYYY-MM-DD-HH',
       zippedArchive: true,
       maxSize: '20m',
-      maxFiles: '14d'
-    })
+      maxFiles: '14d',
+    }),
   ],
 })
 const errorLogger = createLogger({
   level: 'error',
-  format: combine(
-    label({ label: 'HUC' }),
-    timestamp(),
-    myFormat,
-
-  ),
+  format: combine(label({ label: 'HUC' }), timestamp(), myFormat),
   transports: [
     new transports.Console(),
     new DailyRotateFile({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'errors', 'huc%DATE%error.log'),
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'errors',
+        'huc%DATE%error.log'
+      ),
       datePattern: 'YYYY-MM-DD-HH',
       zippedArchive: true,
       maxSize: '20m',
-      maxFiles: '14d'
-    })
+      maxFiles: '14d',
+    }),
   ],
 })
 
